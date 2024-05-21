0fb71ed60c269b4ac08c417b3bc7d72adf790d27
GogaImerlishvili <gogaimerlishvili1@gmail.com>
2024-05-22 00:48:37 +0400

    Saved & create svg && created header

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+import Header from "./components/Header";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  )
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden-hidden">
+        <Header />
+      </div>
+      <ButtonGradient />
+    </>
+  );
 }
 
-export default App
+export default App;
