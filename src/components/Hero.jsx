91deaa1f50510f1d9d75bc159e3a444928f51eee
real0ta <kaxaimerlishvili1@gmail.com>
2024-06-01 16:00:05 +0400

    add temporary background image to component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,19 +5,22 @@ import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
 import { heroIcons } from "../constants";
 import { ScrollParallax } from "react-just-parallax";
 import { useRef } from "react";
+import image from '../images/image1.jpg'
 
 const Hero = () => {
   // const parallaxRef = useRef(null);
   return (
-    <div className="max-w-screen-xl mx-auto flex">
+    <div className="max-w-screen-xl mx-auto flex mt-4">
       <div className="w-2/5  bg-purple-950">
         <h1>Where Gardens Flourish And Dreams Blossom.</h1>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus in pellentesque elit, non ultrices massa.
         </p>
       </div>
-      <div className="w-3/5 bg-blue-950">
-        abc
+      <div className="w-3/5 bg-blue-950 bg-cover bg-center h-[520px]"
+        style={{backgroundImage: `url(${image})`}}
+      >
+        
       </div>
     </div>
   );
