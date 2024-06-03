bf85beee3fa8744a76e93eb6297442cb51cf76fd
real0ta <kaxaimerlishvili1@gmail.com>
2024-06-03 18:34:38 +0400

    add services component

---
+++ src/components/services.jsx
@@ -0,0 +1,17 @@
+import Heading from "./Heading"
+
+const Services = () => {
+  return (
+    <div className="max-w-screen-xl mx-auto">
+      <Heading className="pt-6" title="Choose A Service"/>
+      <div>
+        <article className="w-1/4 h-28 bg-blue-800">
+          abc
+          </article>
+        
+        </div>
+      </div>
+  )
+}
+
+export default Services
