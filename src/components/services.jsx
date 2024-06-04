8afcf6c97b2f4ff592bc542903b7ce0174749ab4
real0ta <kaxaimerlishvili1@gmail.com>
2024-06-04 20:18:05 +0400

    style basic services item

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -2,15 +2,20 @@ import Heading from "./Heading"
 
 const Services = () => {
   return (
-    <div className="max-w-screen-xl mx-auto">
-      <Heading className="pt-6" title="Choose A Service"/>
+    <div className="max-w-screen-xl mx-auto pt-6 pb-6">
+      <Heading title="Choose A Service" />
       <div>
-        <article className="w-1/4 h-28 bg-blue-800">
-          abc
-          </article>
-        
-        </div>
+        <article className="w-1/4 h-28 bg-gradient-to-r from-blue-900 via-blue-900 to-purple-900
+          flex justify-center ">
+          <h1 className="my-auto">ICON</h1>
+          <div className="pl-6">
+            <h2 className="pb-2 pt-4">GARDEN FENCE</h2>
+            <p >Lorem ipsum dolor sit amet, </p>
+          </div>
+        </article>
+
       </div>
+    </div>
   )
 }
 
